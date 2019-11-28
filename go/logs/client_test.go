4794625ec168f1bf4f7c9359fc26b5a7bf7d8215
TJ Holowaychuk <tj@vision-media.ca>
2019-11-28 11:16:56 +0000

    go: add basic benchmark

--- go/logs/client_test.go
+++ go/logs/client_test.go
@@ -1,6 +1,10 @@
 package logs_test
 
 import (
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/apex/logs/go/logs"
@@ -21,4 +25,42 @@ func TestService_GetProjects(t *testing.T) {
 	assert.NotEmpty(t, p.ID)
 	assert.NotEmpty(t, p.Description)
 	assert.NotEmpty(t, p.Name)
+}
+
+// Benchmark the client.
+func Benchmark(b *testing.B) {
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, _ = io.Copy(ioutil.Discard, r.Body)
+	}))
+	defer s.Close()
+
+	c := logs.Client{URL: s.URL}
+	count := 100
+
+	var events []logs.Event
+	for i := 0; i < count; i++ {
+		events = append(events, logs.Event{
+			Level:   "info",
+			Message: "Some message here",
+			Fields: map[string]interface{}{
+				"region":  "us-west-2",
+				"project": "api",
+				"version": "v1.0.0",
+			},
+		})
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+	b.SetBytes(int64(count))
+
+	for i := 0; i < b.N; i++ {
+		err := c.AddEvents(logs.AddEventsInput{
+			ProjectID: "testing",
+			Events:    events,
+		})
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
 }
