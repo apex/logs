193e95a2882b2ed45fec6fa95749f8e70289555d
TJ Holowaychuk <tj@vision-media.ca>
2020-05-27 10:10:46 +0100

    go: add AUTH_TOKEN env var for testing

--- go/logs/client_test.go
+++ go/logs/client_test.go
@@ -1,10 +1,7 @@
 package logs_test
 
 import (
-	"io"
-	"io/ioutil"
-	"net/http"
-	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/apex/logs/go/logs"
@@ -14,7 +11,8 @@ import (
 // Test GetProjects.
 func TestService_GetProjects(t *testing.T) {
 	c := logs.Client{
-		URL: "http://localhost:5001",
+		URL:       "http://localhost:5001",
+		AuthToken: os.Getenv("AUTH_TOKEN"),
 	}
 
 	res, err := c.GetProjects()
@@ -25,42 +23,4 @@ func TestService_GetProjects(t *testing.T) {
 	assert.NotEmpty(t, p.ID)
 	assert.NotEmpty(t, p.Description)
 	assert.NotEmpty(t, p.Name)
-}
-
-// Benchmark the client.
-func Benchmark(b *testing.B) {
-	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, _ = io.Copy(ioutil.Discard, r.Body)
-	}))
-	defer s.Close()
-
-	c := logs.Client{URL: s.URL}
-	count := 100
-
-	var events []logs.Event
-	for i := 0; i < count; i++ {
-		events = append(events, logs.Event{
-			Level:   "info",
-			Message: "Some message here",
-			Fields: map[string]interface{}{
-				"region":  "us-west-2",
-				"project": "api",
-				"version": "v1.0.0",
-			},
-		})
-	}
-
-	b.ResetTimer()
-	b.ReportAllocs()
-	b.SetBytes(int64(count))
-
-	for i := 0; i < b.N; i++ {
-		err := c.AddEvents(logs.AddEventsInput{
-			ProjectID: "testing",
-			Events:    events,
-		})
-		if err != nil {
-			b.Fatal(err)
-		}
-	}
 }
